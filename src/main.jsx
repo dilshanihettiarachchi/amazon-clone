1fbc2955ebcedc7320b4ac6c956025e058c0da77
Dilshani Hettiarachchi <dilshani.madhuwanthi151@gmail.com>
2025-01-30 13:45:24 +0530

    add reducer and stateProvider

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,15 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
+import { StateProvider } from './StateProvider.jsx'
+import reducer, { initialState } from './reducer.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+      <StateProvider initialState={initialState} reducer={reducer}>
+        <App />
+      </StateProvider>
     </BrowserRouter>
   </StrictMode>,
 )
---
+++ src/reducer.jsx
@@ -0,0 +1,31 @@
+export const initialState = {
+  cart: [{
+    id: "123",
+    title: "50 Pcs Golden Paper Placemats - 11 x 16.5 Inch Gold Disposable Paper Place Mats with Coasters Vintage Golden Leaves Table Mats for Wedding Anniversary Holiday Party Kitchen Dining Table Decor",
+    price: 15.99,
+    rating: 5,
+    image: "https://m.media-amazon.com/images/I/81sk-ru9giL._AC_SX569_.jpg"
+  }],
+};
+
+export const getCartTotal = (cart) => 
+  cart?.reduce((amount, item) => item.price + amount, 0);
+
+function reducer(state, action) {
+  switch(action.type) {
+    case 'ADD_TO_Cart':
+      return {
+        ...state, 
+        cart: [...state.cart, action.item],
+      };
+    case 'REMOVE_FROM_Cart':
+      return {
+        ...state,
+        cart: state.cart.filter((item) => item.id !== action.id),
+      };
+    default:
+      return state;
+  }
+}
+
+export default reducer;
